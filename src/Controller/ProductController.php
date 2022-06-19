f4bb68bb2e76fa9a797494819f00a5a0ef986e14
Hazem Marzouki <m.hazem1812@myu.universitecentrale.tn>
2022-06-19 22:58:51 +0200

    Add product filtering and search

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,29 +2,41 @@
 
 namespace App\Controller;
 
+use App\Classe\Search;
 use App\Entity\Product;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\SearchType;
+use Symfony\Component\HttpFoundation\Request;
 
 class ProductController extends AbstractController
 {
     private $entityManager;
     public function __construct(EntityManagerInterface $entityManager)
-    {   
-        $this->entityManager= $entityManager;
-        
+    {
+        $this->entityManager = $entityManager;
     }
 
     #[Route('/produits', name: 'products')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        $products = $this->entityManager->getRepository(Product::class)->findAll();
+
+        $search = new Search();
+        $form = $this->createForm(SearchType::class, $search);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $products = $this->entityManager->getRepository(Product::class)->findWithSearch($search);
+        } else {
+            $products = $this->entityManager->getRepository(Product::class)->findAll();
+        }
 
 
-        return $this->render('product/index.html.twig' , [
-            'products'=>$products
+        return $this->render('product/index.html.twig', [
+            'products' => $products,
+            'form' => $form->createView()
         ]);
     }
 
@@ -33,12 +45,12 @@ class ProductController extends AbstractController
     {
         $product = $this->entityManager->getRepository(Product::class)->findOneBySlug($slug);
 
-            if(!$product) {
-                return $this->redirectToRoute('products');
-            }
+        if (!$product) {
+            return $this->redirectToRoute('products');
+        }
 
-        return $this->render('product/show.html.twig' , [
-            'product'=>$product
+        return $this->render('product/show.html.twig', [
+            'product' => $product
         ]);
     }
 }
