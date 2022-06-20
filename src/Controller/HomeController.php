1294f2b10c2fd2102b55723fd12f5a167601f2c7
Hazem Marzouki <m.hazem1812@myu.universitecentrale.tn>
2022-06-20 02:47:44 +0200

    Add shopping cart

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,9 +10,10 @@ class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
     public function index(): Response
-    {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+    {   
+        
+
+
+        return $this->render('home/index.html.twig');
     }
 }
