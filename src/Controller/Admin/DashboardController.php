79ebbdf67a69431c4f5a35debfdbd707e376a7e7
Hazem Marzouki <m.hazem1812@myu.universitecentrale.tn>
2022-06-22 19:45:04 +0200

    add carrier entity

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -7,6 +7,7 @@ use App\Entity\Headers;
 use App\Entity\Product;
 use App\Entity\Category;
 use App\Entity\SamplePack;
+use App\Entity\Carrier;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -52,5 +53,7 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToCrud('Categories', 'fas fa-list', Category::class);
         yield MenuItem::linkToCrud('Produits', 'fas fa-tag', Product::class);
         yield MenuItem::linkToCrud('Headers', 'fas fa-desktop',  Headers::class);
+        yield MenuItem::linkToCrud('Carriers', 'fas fa-truck',  Carrier::class);
+
     }
 }
